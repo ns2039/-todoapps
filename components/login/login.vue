f5b76338f750777dc78bf13ad09f958b035805c4
shogonino <nino.showgo@gmail.com>
2020-06-01 18:28:07 +0900

    first commit

---
+++ components/login/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- <section class="hero is-light is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <article v-show="errorMessage" class="message is-warning">
+            <div class="message-body">
+              {{ $t(errorMessage) }}
+            </div>
+          </article>
+          <div class="column is-4 is-offset-4">
+            <div class="box">
+               <div class="field">
+                 <div class="control">
+                   <input class="input is-large" type="email" placeholder="Eメール" v-model="email" autofocus="" v-validate="'required|email'" name="email">
+                 </div>
+              </div>
+              <div class="field">
+                <div class="control">
+                  <input class="input is-large" type="password" placeholder="パスワード" v-model="password" v-validate="'required|min:6|max:20'" maxlength="20" name="password">
+                </div>
+              </div>
+              <div class="field">
+                <label class="checkbox">
+                <input type="checkbox">
+                ログインしたままにする
+                </label>
+               </div>
+               <button class="button is-block is-info is-large is-fullwidth" @click="login()" :disabled="!isValidated" >„É≠„Ç∞„Ç§„É≥</button>
+              </div>
+              <p class="has-text-grey">
+                <a href="..">パスワードを忘れた方はこちら</a>
+              </p>
+            </div>
+          </div>
+        </div>
+    </section> -->
+    <p> aaa</p>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <b-navbar toggleable="lg" type="light" variant="success">
+        <b-navbar-brand href="#" thread-class="text-white">Todo List</b-navbar-brand>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                  <em>User</em>
+                </template>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+    <table class="Lists">
+      <thead>
+        <tr>
+          <th>タスク</th>
+          <th>登録日時</th>
+          <th>状態</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>テスト</td>
+          <td>2020-06-1 17:00</td>
+          <td><button class="button button--edit">編集</button></td>
+          <td><button class="button button--delete">削除</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="addArea">
+      <input type="text" name="addName" id="addName" placeholder="タスクを入力してください">
+      <button id="addButton" class="button button--green">追加</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import Login from '~/components/login/login.vue'
+
+export default {
+  components: {
+    Login,
+  }
+}
+</script>
+
+<style>
+</style>
